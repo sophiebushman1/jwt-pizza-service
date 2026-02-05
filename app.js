d19392db90a09ec0e4858242118f6e1dbb29e7ff
SophiaTaylor7 <sophia7taylor@gmail.com>
2026-02-04 20:17:41 -0700

    fixed app

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// src/app.js
 const express = require('express');
 const app = express();
 
+// routes (add only the ones your backend actually has)
 const authRouter = require('./routes/authRouter');
 const orderRouter = require('./routes/orderRouter');
-const userRouter = require('./routes/userRouter');
 const franchiseRouter = require('./routes/franchiseRouter');
+const userRouter = require('./routes/userRouter');
 
 app.use('/auth', authRouter);
 app.use('/order', orderRouter);
-app.use('/user', userRouter);
 app.use('/franchise', franchiseRouter);
-
-app.get('/', (req, res) => res.send('Service is running'));
+app.use('/user', userRouter);
 
 module.exports = app;
