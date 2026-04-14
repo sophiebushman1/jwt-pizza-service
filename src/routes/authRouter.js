f0f3b131238a790615fb10b49ef8d53e981b79a8
Sophie <sophia7taylor@gmail.com>
2026-04-13 22:34:37 -0600

    del 12 implimentation

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -101,7 +101,7 @@ authRouter.delete(
 );
 
 async function setAuth(user) {
-  const token = jwt.sign(user, config.jwtSecret);
+  const token = jwt.sign(user, config.jwtSecret, { expiresIn: '1d' });
   await DB.loginUser(user.id, token);
   return token;
 }
